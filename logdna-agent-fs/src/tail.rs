a0fcf7f45ecf31f9c0a48f3cea48cfb7b9c5c7d9
Connor Peticca <cjpet10@gmail.com>
2019-05-21 14:30:54 -0400

    added docs to tail module
    Summary: I added docs to the tail module, looking for review of the docs.
    
    Test Plan: Local
    
    Reviewers: #rust, smusali
    
    Reviewed By: smusali
    
    Subscribers: smusali
    
    Differential Revision: https://logdna.phacility.com/D487

--- logdna-agent-fs/src/tail.rs
+++ logdna-agent-fs/src/tail.rs
@@ -5,21 +5,22 @@ use std::path::PathBuf;
 
 use crossbeam::{Receiver, Sender, unbounded};
 
+use agent_core::http::body::LineBuilder;
+
 use crate::Event;
 
-pub struct Line {
-    pub line: String,
-    pub file: String,
-    pub length: u64,
-}
-
+/// Tails files on a filesystem by inheriting events from a Watcher
 pub struct Tailer {
+    // the sender that we are going to share to threads that want to communicate e.g watcher thread
     event_sender: Sender<Event>,
+    // used to pops items out of the sender
     event_receiver: Receiver<Event>,
+    // tracks the offset (bytes from the beginning of the file we have read) of file(s)
     offsets: HashMap<PathBuf, u64>,
 }
 
 impl Tailer {
+    /// Creates new instance of Tailer
     pub fn new() -> Self {
         let (s, r) = unbounded();
         Self {
@@ -28,27 +29,33 @@ impl Tailer {
             offsets: HashMap::new(),
         }
     }
-
+    /// Returns the sender the tailer is "listening" on
     pub fn sender(&self) -> Sender<Event> {
         self.event_sender.clone()
     }
-
-    pub fn run(mut self, sender: Sender<Line>) {
+    /// Runs the main logic of the tailer, this can only be run once so Tailer is consumed
+    pub fn run(mut self, sender: Sender<LineBuilder>) {
         loop {
+            // safe to unwrap
             let event = self.event_receiver.recv().unwrap();
 
             match event {
                 Event::Initiate(path) => {
+                    // will initiate a file to it's current length
                     let len = path.metadata().map(|m| m.len()).unwrap_or(0);
                     info!("initiated {:?} to offset table with offset {}", path, len);
                     self.offsets.insert(path, len);
                 }
                 Event::New(path) => {
+                    // similar to initiate but sets the offset to 0
                     info!("added {:?} to offset table", path);
                     self.offsets.insert(path.clone(), 0);
                     self.tail(path, &sender);
                 }
                 Event::Delete(ref path) => {
+                    // just remove the file from the offset table on delete
+                    // this acts almost like a garbage collection mechanism
+                    // ensuring the offset table doesn't "leak" by holding deleted files
                     info!("removed {:?} from offset table", path);
                     self.offsets.remove(path);
                 }
@@ -56,8 +63,9 @@ impl Tailer {
             }
         }
     }
-
-    fn tail(&mut self, path: PathBuf, sender: &Sender<Line>) {
+    // tail a file for new line(s)
+    fn tail(&mut self, path: PathBuf, sender: &Sender<LineBuilder>) {
+        // get the offset from the map, return if not found
         let offset = match self.offsets.get_mut(&path) {
             Some(v) => v,
             None => {
@@ -65,8 +73,9 @@ impl Tailer {
                 return;
             }
         };
-
+        // get the name of the file set to "" if the file is invalid utf8
         let file_name = path.to_str().unwrap_or("").to_string();
+        // open the file, create a reader and grab the file length
         //todo when match postfix lands on stable replace prefix match for readability
         let (mut reader, len) = match File::open(&path)
             .and_then(|f| f.metadata().map(|m| (f, m)))
@@ -77,17 +86,18 @@ impl Tailer {
                 return;
             }
         };
-
+        // if the offset is greater than the file's len
+        // it's very likely a truncation occurred
         if *offset > len {
             info!("{:?} was truncated from {} to {}", path, offset, len);
             *offset = len;
             return;
         }
-
+        // if we are at the end of the file there's no work to do
         if *offset == len {
             return;
         }
-
+        // seek to the offset, this creates the "tailing" effect
         if let Err(e) = reader.seek(SeekFrom::Start(*offset)) {
             error!("error seeking {:?}", e);
             return;
@@ -95,7 +105,7 @@ impl Tailer {
 
         loop {
             let mut raw_line = Vec::new();
-
+            // read until a new line returning the line length
             let line_len = match reader.read_until(b'\n', &mut raw_line) {
                 Ok(v) => v as u64,
                 Err(e) => {
@@ -104,24 +114,28 @@ impl Tailer {
                     ;
                 }
             };
-
+            // try to parse the raw data as utf8
+            // if that fails replace invalid chars with blank chars
+            // see String::from_utf8_lossy docs
             let mut line = String::from_utf8(raw_line)
                 .unwrap_or_else(|e|
                     String::from_utf8_lossy(e.as_bytes()).to_string()
                 );
-
+            // if the line doesn't end with a new line we might have read in the middle of a write
+            // so we return in this case
             if !line.ends_with('\n') {
                 return;
             }
+            // remove the trailing new line
             line.pop();
-
+            // increment the offset
             *offset += line_len;
-
-            sender.send(Line{
-                line,
-                file: file_name.clone(),
-                length: line_len,
-            }).unwrap()
+            // send the line upstream, safe to unwrap
+            sender.send(
+                LineBuilder::new()
+                    .line(line)
+                    .file(file_name.clone())
+            ).unwrap()
         }
     }
 }
