e73ccdacaa7f8fdf6afb6423ca7432cb16d058ed
Connor Peticca <cjpet10@gmail.com>
2019-05-24 08:29:17 -0400

    limited runtime to 1 thread

--- logdna-agent-http/src/client.rs
+++ logdna-agent-http/src/client.rs
@@ -1,4 +1,4 @@
-use tokio::runtime::Runtime;
+use tokio::runtime::{Builder, Runtime};
 
 use agent_core::http::client::Client as HttpClient;
 use agent_core::http::request::RequestTemplate;
@@ -10,7 +10,10 @@ pub struct Client {
 
 impl Client {
     pub fn new(template: RequestTemplate) -> Self {
-        let mut runtime = Runtime::new().expect("Runtime::new()");
+        let mut runtime = Builder::new()
+            .core_threads(1)
+            .build()
+            .expect("Runtime::new()");
         Self {
             inner: HttpClient::new(template, &mut runtime),
             runtime,
